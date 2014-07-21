3fb8daa5eb3ebe649cb60e1954c05280fe906362
Phillip Gutowski <phillip.gutowski@gmail.com>
2014-07-21 15:32:07 -0400

    Create post form, display all posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,24 @@
 class PostsController < ApplicationController
   def index
     @posts = Post.all
+    @post = Post.new
+  end
+
+  def create
+    @post = current_user.posts.new(post_params)
+
+    if @post.save
+      render @post
+    end
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(
+      :title,
+      :price,
+      :body,
+    )
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,9 +3,10 @@ require "monban/constraints/signed_in"
 Rails.application.routes.draw do
   resource :session, only: [:new, :create, :destroy]
   resources :users, only: [:new, :create]
+  resources :posts, only: [:create]
 
   constraints Monban::Constraints::SignedIn.new do
-    root "posts#index", as: :posts
+    root "posts#index", as: :posts_index
   end
 
 
