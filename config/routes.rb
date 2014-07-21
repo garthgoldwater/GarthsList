137d763843751e7dfc854f79b776ba81353be437
Phillip Gutowski <phillip.gutowski@gmail.com>
2014-07-21 14:05:26 -0400

    Create post index view add routes for signed in user

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,14 @@
+require "monban/constraints/signed_in"
+
 Rails.application.routes.draw do
   resource :session, only: [:new, :create, :destroy]
   resources :users, only: [:new, :create]
+
+  constraints Monban::Constraints::SignedIn.new do
+    root "posts#index", as: :posts
+  end
+
+
 
   root to: "sessions#new"
 end
