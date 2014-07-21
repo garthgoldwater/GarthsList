c551a1e8ee303284a2e957524c08bebfe71aca91
Phillip Gutowski <phillip.gutowski@gmail.com>
2014-07-21 13:01:56 -0400

    Add Region, Category and Post tables

---
+++ db/migrate/20140721164853_create_posts.rb
@@ -0,0 +1,17 @@
+class CreatePosts < ActiveRecord::Migration
+  def change
+    create_table :posts do |t|
+      t.string :title, null: false
+      t.text :body, null: false
+      t.integer :price
+      t.references :region, index: true, null: false
+      t.references :category, index: true, null: false
+      t.references :user, index: true, null: false
+
+      t.timestamps
+    end
+    add_index :posts, :title
+    add_index :posts, :body
+    add_index :posts, :price
+  end
+end
