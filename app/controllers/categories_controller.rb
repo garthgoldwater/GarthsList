133cc3758dd9ed6e17b9053be0f2224251a750ee
Garth Goldwater <garth.goldwater@gmail.com>
2014-07-22 14:01:28 -0400

    remove duplicate show action, remove relic of bugfixing

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -17,11 +17,6 @@ class CategoriesController < ApplicationController
     end
   end
 
-  def show
-    @category = Category.find(params[:id])
-    @posts = @category.posts
-  end
-
   private
 
   def category_params
