a69469c37dfa12812a10e73195c25ae117b9606d
Phillip Gutowski <phillip.gutowski@gmail.com>
2014-07-22 11:38:43 -0400

    User can assign region to new post

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140722141847) do
+ActiveRecord::Schema.define(version: 20140722153537) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -32,9 +32,11 @@ ActiveRecord::Schema.define(version: 20140722141847) do
     t.datetime "created_at",              null: false
     t.datetime "updated_at",              null: false
     t.integer  "category_id",             null: false
+    t.integer  "region_id",   default: 0, null: false
   end
 
   add_index "posts", ["category_id"], name: "index_posts_on_category_id", using: :btree
+  add_index "posts", ["region_id"], name: "index_posts_on_region_id", using: :btree
   add_index "posts", ["user_id"], name: "index_posts_on_user_id", using: :btree
 
   create_table "regions", force: true do |t|
