dd2e78c7f587e71c4f8c09d13b0171eedced9584
Garth Goldwater <garth.goldwater@gmail.com>
2014-07-23 15:20:01 -0400

    getting views set up for update

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ require "monban/constraints/signed_in"
 Rails.application.routes.draw do
   resource :session, only: [:new, :create, :destroy]
   resources :users, only: [:new, :create]
-  resources :posts, only: [:new, :create, :index, :show, :destroy]
+  resources :posts, only: [:new, :create, :index, :show, :destroy, :update]
   resources :categories, only: [:new, :create, :index, :show]
   resource :admin, only: [:show]
   resource :landing, only: [:show]
