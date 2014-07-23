4066936957941b1da4b07b2a2aa839ad8eb507e0
Garth Goldwater <garth.goldwater@gmail.com>
2014-07-23 17:43:18 -0400

    change <%= to <% for if statement

--- app/controllers/spams_controller.rb
+++ app/controllers/spams_controller.rb
@@ -1,7 +1,12 @@
 class SpamsController < ApplicationController
+  before_action :require_admin, only: [:create]
   def create
     post = Post.find(params[:post_id])
     post.spam = false
     redirect_to post
+  end
+
+  def index
+    @posts_marked_spam = Post.find_by(spam: true)
   end
 end
