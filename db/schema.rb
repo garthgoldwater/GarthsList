6de9e04cdc4a65a2956f44620da71c2b54f6ce89
Phillip Gutowski <phillip.gutowski@gmail.com>
2014-07-24 15:00:55 -0400

    Migrate db

--- db/schema.rb
+++ db/schema.rb
@@ -11,11 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-<<<<<<< HEAD
-ActiveRecord::Schema.define(version: 20140723213156) do
-=======
 ActiveRecord::Schema.define(version: 20140723220101) do
->>>>>>> Add image to post
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
